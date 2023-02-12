59f77c9ecdac5e5b28ff488cb0dd0902782b1323
Jordan Bonecutter <i@jordanbonecutter.com>
2023-02-12 08:16:58 -0800

    github-actions::add gofmt (#4)
    * add gofmt action
    
    * use go fmt
    
    * remove flags
    
    * commit style changes
    
    * newer checkout action
    
    * same as purplecrayon yaml
    
    * Linter found style changes
    
    ---------
    
    Co-authored-by: jordan-bonecutter <jordan-bonecutter@users.noreply.github.com>

--- json_test.go
+++ json_test.go
@@ -1,59 +1,58 @@
 package goption
 
 import (
-  "testing"
-  "encoding/json"
+	"encoding/json"
+	"testing"
 )
 
 type Bar struct {
-  Baz string
+	Baz string
 }
 
 type Foo struct {
-  Stuff Option[Bar]
-  Things []int
+	Stuff  Option[Bar]
+	Things []int
 }
 
-
 func TestJSONMarshal(t *testing.T) {
-  foo := Foo{
-    Things: []int{1, 2, 3},
-  }
-  encoded, err := json.Marshal(foo)
-  if err != nil {
-    t.Fatalf("Failed marshalling json: %s", err)
-  }
+	foo := Foo{
+		Things: []int{1, 2, 3},
+	}
+	encoded, err := json.Marshal(foo)
+	if err != nil {
+		t.Fatalf("Failed marshalling json: %s", err)
+	}
 
-  if string(encoded) != `{"Stuff":null,"Things":[1,2,3]}` {
-    t.Errorf("Unexpected encoded data: %s", string(encoded))
-  }
+	if string(encoded) != `{"Stuff":null,"Things":[1,2,3]}` {
+		t.Errorf("Unexpected encoded data: %s", string(encoded))
+	}
 
-  foo.Stuff = Some(Bar{Baz: "hey!"})
-  encoded, err = json.Marshal(foo)
-  if err != nil {
-    t.Fatalf("Failed marshalling json: %s", err)
-  }
+	foo.Stuff = Some(Bar{Baz: "hey!"})
+	encoded, err = json.Marshal(foo)
+	if err != nil {
+		t.Fatalf("Failed marshalling json: %s", err)
+	}
 
-  if string(encoded) != `{"Stuff":{"Baz":"hey!"},"Things":[1,2,3]}` {
-    t.Errorf("Unexpected encoded data: %s", string(encoded))
-  }
+	if string(encoded) != `{"Stuff":{"Baz":"hey!"},"Things":[1,2,3]}` {
+		t.Errorf("Unexpected encoded data: %s", string(encoded))
+	}
 }
 
 func TestJSONUnmarshal(t *testing.T) {
-  var foo Foo
-  if err := json.Unmarshal([]byte(`{"Stuff":null,"Things":[1,2,3]}`), &foo); err != nil {
-    t.Errorf("Failed unmarshalling into foo: %s", err)
-  } else if len(foo.Things) != 3 || foo.Things[0] != 1 || foo.Things[1] != 2 || foo.Things[2] != 3 {
-    t.Errorf("Failed unmarshalling foo.Things: %v", foo.Things)
-  } else if foo.Stuff.Ok() {
-    t.Errorf("Expected optional value to be empty.")
-  }
+	var foo Foo
+	if err := json.Unmarshal([]byte(`{"Stuff":null,"Things":[1,2,3]}`), &foo); err != nil {
+		t.Errorf("Failed unmarshalling into foo: %s", err)
+	} else if len(foo.Things) != 3 || foo.Things[0] != 1 || foo.Things[1] != 2 || foo.Things[2] != 3 {
+		t.Errorf("Failed unmarshalling foo.Things: %v", foo.Things)
+	} else if foo.Stuff.Ok() {
+		t.Errorf("Expected optional value to be empty.")
+	}
 
-  if err := json.Unmarshal([]byte(`{"Stuff":{"Baz":"hey!"},"Things":[1,2,3]}`), &foo); err != nil {
-    t.Errorf("Failed unmarshalling into foo: %s", err)
-  } else if len(foo.Things) != 3 || foo.Things[0] != 1 || foo.Things[1] != 2 || foo.Things[2] != 3 {
-    t.Errorf("Failed unmarshalling foo.Things: %v", foo.Things)
-  } else if !foo.Stuff.Ok() || foo.Stuff.Unwrap().Baz != "hey!" {
-    t.Errorf("Expected optional value to be present.")
-  }
+	if err := json.Unmarshal([]byte(`{"Stuff":{"Baz":"hey!"},"Things":[1,2,3]}`), &foo); err != nil {
+		t.Errorf("Failed unmarshalling into foo: %s", err)
+	} else if len(foo.Things) != 3 || foo.Things[0] != 1 || foo.Things[1] != 2 || foo.Things[2] != 3 {
+		t.Errorf("Failed unmarshalling foo.Things: %v", foo.Things)
+	} else if !foo.Stuff.Ok() || foo.Stuff.Unwrap().Baz != "hey!" {
+		t.Errorf("Expected optional value to be present.")
+	}
 }
--- option.go
+++ option.go
@@ -2,104 +2,103 @@ package goption
 
 // Option represents a value whose presence is optional.
 type Option[T any] struct {
-  t T
-  ok bool
+	t  T
+	ok bool
 }
 
 // Unwrap forcefully unwraps the Optional value.
 // If the optional is not ok this function will panic.
 func (o Option[T]) Unwrap() T {
-  return o.Expect("Unwrapped empty optional")
+	return o.Expect("Unwrapped empty optional")
 }
 
 // UnwrapRef returns a reference to the underlying T.
 func (o *Option[T]) UnwrapRef() *T {
-  return o.ExpectRef("Unwrapped empty optional")
+	return o.ExpectRef("Unwrapped empty optional")
 }
 
 // Expect unwraps o and panics with msg if it's empty.
 func (o Option[T]) Expect(msg string) T {
-  if !o.ok {
-    panic(msg)
-  }
+	if !o.ok {
+		panic(msg)
+	}
 
-  return o.t
+	return o.t
 }
 
 // ExpectRef unwraps o and panics with msg if it's empty.
 func (o *Option[T]) ExpectRef(msg string) *T {
-  if !o.ok {
-    panic(msg)
-  }
+	if !o.ok {
+		panic(msg)
+	}
 
-  return &o.t
+	return &o.t
 }
 
 // UnwrapOr unwraps the optional if it's present, otherwise it returns default.
 func (o Option[T]) UnwrapOr(def T) T {
-  if !o.ok {
-    return def
-  }
+	if !o.ok {
+		return def
+	}
 
-  return o.t
+	return o.t
 }
 
-// UnwrapOrDefault unwraps T if it's present, otherwise it returns the default 
+// UnwrapOrDefault unwraps T if it's present, otherwise it returns the default
 // value for T.
 func (o Option[T]) UnwrapOrDefault() T {
-  var def T
-  return o.UnwrapOr(def)
+	var def T
+	return o.UnwrapOr(def)
 }
 
 // Ok returns if the optional is present.
 func (o Option[T]) Ok() bool {
-  return o.ok
+	return o.ok
 }
 
 // Some returns an Option whose underlying value is present.
 func Some[T any](t T) Option[T] {
-  return Option[T]{
-    t: t,
-    ok: true,
-  }
+	return Option[T]{
+		t:  t,
+		ok: true,
+	}
 }
 
 // None returns an empty optional value.
 func None[T any]() Option[T] {
-  return Option[T]{
-    ok: false,
-  }
+	return Option[T]{
+		ok: false,
+	}
 }
 
 // FromRef returns an Option whose underlying value is present if t is not nil.
 // Otherwise, it returns an empty optional value.
 func FromRef[T any](t *T) Option[T] {
-  if t == nil {
-    return None[T]()
-  }
-  return Some(*t)
+	if t == nil {
+		return None[T]()
+	}
+	return Some(*t)
 }
 
 // Apply f to the optional value.
 func Apply[In, Out any](in Option[In], f func(In) Out) Option[Out] {
-  if !in.ok {
-    return None[Out]()
-  }
+	if !in.ok {
+		return None[Out]()
+	}
 
-  return Some(f(in.t))
+	return Some(f(in.t))
 }
 
 // Do runs the function f which may panic.
 // If f does not panic Some(f()) is returned.
 // Otherwise none is returned.
 func Do[T any](f func() T) (o Option[T]) {
-  defer func() {
-    if r := recover(); r != nil {
-      o = None[T]()
-    }
-  }()
+	defer func() {
+		if r := recover(); r != nil {
+			o = None[T]()
+		}
+	}()
 
-  o = Some(f())
-  return
+	o = Some(f())
+	return
 }
-
